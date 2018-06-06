ff71d511e21c2574896b83c82b13e68382c9856d
Sumit Paul <paulsumit1993@gmail.com>
2018-06-06 21:10:08 +0530

    Add doc.

--- Natural Language Demo.playground/Contents.swift
+++ Natural Language Demo.playground/Contents.swift
@@ -1,15 +1,24 @@
 // Run on macOS Mojave Beta (10.14+)
-
 import NaturalLanguage
 
-let text = <#language string#> // example string: "একটি মহান পাহাড় আরোহণ করার পরে, শুধুমাত্র এক যে আরো অনেক পাহাড় আরোহণ আছে খুঁজে পাওয়া যায় নি।"
+let text = <#test_string#> // example string: "একটি মহান পাহাড় আরোহণ করার পরে, শুধুমাত্র এক যে আরো অনেক পাহাড় আরোহণ আছে খুঁজে পাওয়া যায় নি।"
 
+/// Finds the most likely language for the text provided as input.
+///
+/// - Parameter text: The text to analyze.
+/// - Returns: The most likely language of the text.
 func findDominantlanguage(for text: String) -> NLLanguage? {
     let naturalLanguageRecognizer = NLLanguageRecognizer()
     naturalLanguageRecognizer.processString(text)
     return naturalLanguageRecognizer.dominantLanguage
 }
 
+/// Breaks the input string into tokens based on the inputs
+///
+/// - Parameters:
+///   - text: The text to be tokenized.
+///   - unit: The linguistic unit that this tokenizer uses. https://developer.apple.com/documentation/naturallanguage/nltokenizer/2998450-unit
+/// - Returns: A dictionary of the token and hints about the contents of the string for the tokenizer.
 func tokenize(string text: String, using unit: NLTokenUnit) -> [String: NLTokenizer.Attributes] {
     let tokenizer = NLTokenizer(unit: unit)
     tokenizer.string = text
@@ -21,6 +30,13 @@ func tokenize(string text: String, using unit: NLTokenUnit) -> [String: NLTokeni
     return tokensAndAttributes
 }
 
+/// Function to classify nouns, verbs, adjectives, and other parts of speech in a string.
+///
+/// - Parameters:
+///   - text: The text to be classified.
+///   - tagSchemes: The tag schemes configured for this linguistic tagger.
+///   - tagOptions: Constants for linguistic tagger enumeration specifying which tokens to omit and whether to join names.
+/// - Returns: A dictionary of the token and noun, verb, adjective, and other parts of speech in the token.
 func determinePartsofSpeech(of text: String, with tagSchemes: [NLTagScheme], and tagOptions: NLTagger.Options) -> [String: String] {
     let tagger = NLTagger(tagSchemes: tagSchemes)
     tagger.string = text
